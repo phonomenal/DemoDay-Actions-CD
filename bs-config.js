26bf14bcc29b4785e597a2ea33ace88bde4ad580
James Ha <james-leha@github.com>
2020-12-20 16:50:54 -0500

    first commit

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    proxy: "localhost:8000",
+    files: ["**/*.css", "**/*.pug", "**/*.js"],
+    ignore: ["node_modules"],
+    reloadDelay: 10,
+    ui: false,
+    notify: false,
+    port: 3000,
+  };
