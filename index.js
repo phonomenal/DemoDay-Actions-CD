5dc623b3d16c5b68dc19b903c09bc63dfe548c04
James Ha <james-leha@github.com>
2021-01-05 09:42:31 -0500

    npx standard fixes 1

--- index.js
+++ index.js
@@ -4,26 +4,25 @@
  * Required External Modules
  */
 
-const express = require("express");
-var bodyParser = require('body-parser')
-const path = require("path");
-const { Octokit } = require("@octokit/rest");
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const { Octokit } = require('@octokit/rest')
 
 /**
  * App Variables
  */
 
-const app = express();
-const port = process.env.PORT || "8000";
-
+const app = express()
+const port = process.env.PORT || '8000'
 
 /**
  *  App Configuration
  */
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -31,111 +30,107 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
 /**
  * Routes Definitions
  */
 
-app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
-  });
-
-
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' })
+})
 
 app.post('/user', async (req, res) => {
+  // try
+  // {
+  //     const username = req.body.username
+  //     //...
+  //     var userData = await getUser(username)
 
-    // try
-    // {
-    //     const username = req.body.username
-    //     //...
-    //     var userData = await getUser(username)
-        
-    //     var user_created_at = userData.created_at
+  //     var user_created_at = userData.created_at
 
-    //     var created_at_dateFormat = user_created_at.split('T')
-    //     var dateFormatted = created_at_dateFormat[0]
+  //     var created_at_dateFormat = user_created_at.split('T')
+  //     var dateFormatted = created_at_dateFormat[0]
 
-    //     res.render("user", { title: "Profile", userProfile: 
-    //     { handle: userData.login, avatar_url: userData.avatar_url, bio: userData.bio, html_url: userData.html_url, 
-    //         company: userData.company, location: userData.location, created_at: dateFormatted} });
-    // }
-    // catch(error) 
-    // {
-    //     res.render(path.join(__dirname + '/views/404.pug'), {error: error});
-    // }
+  //     res.render("user", { title: "Profile", userProfile:
+  //     { handle: userData.login, avatar_url: userData.avatar_url, bio: userData.bio, html_url: userData.html_url,
+  //         company: userData.company, location: userData.location, created_at: dateFormatted} });
+  // }
+  // catch(error)
+  // {
+  //     res.render(path.join(__dirname + '/views/404.pug'), {error: error});
+  // }
 
-    res.redirect(303, '/user/' + req.body.username)
+  res.redirect(303, '/user/' + req.body.username)
 })
 
 app.get('/user/:handle', async (req, res) => {
-    console.log("GET - requesting user handle: " + req.params.handle)
+  console.log('GET - requesting user handle: ' + req.params.handle)
 
+  const username = req.params.handle
 
-    var username = req.params.handle
-    
-    var userData = await getUser(username, res)
-    
-    if (userData)
-    {
-        var user_created_at = userData.created_at
+  const userData = await getUser(username, res)
 
-        var created_at_dateFormat = user_created_at.split('T')
-        var dateFormatted = created_at_dateFormat[0]
+  if (userData) {
+    const user_created_at = userData.created_at
 
-        res.render("user", { title: userData.login, userProfile: 
-        { handle: userData.login, avatar_url: userData.avatar_url, bio: userData.bio, html_url: userData.html_url, 
-            company: userData.company, location: userData.location, created_at: dateFormatted} });
-    }
+    const created_at_dateFormat = user_created_at.split('T')
+    const dateFormatted = created_at_dateFormat[0]
 
+    res.render('user', {
+      title: userData.login,
+      userProfile:
+        {
+          handle: userData.login,
+          avatar_url: userData.avatar_url,
+          bio: userData.bio,
+          html_url: userData.html_url,
+          company: userData.company,
+          location: userData.location,
+          created_at: dateFormatted
+        }
+    })
+  }
 })
 
-//404 not found when no match - included at the end of all routes
-app.get('*', function(req, res){
-    res.render(path.join(__dirname + '/views/404.pug'), {error: 'Page Not Found'});
-});
+// 404 not found when no match - included at the end of all routes
+app.get('*', function (req, res) {
+  res.render(path.join(__dirname + '/views/404.pug'), { error: 'Page Not Found' })
+})
 
 /**
  * Server Activation
  */
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-  });
-
+  console.log(`Listening to requests on http://localhost:${port}`)
+})
 
 /**
  * Business Logic
  */
 
-const octokit = new Octokit();
+const octokit = new Octokit()
 
-async function getUser(userHandle, res)
-{
-    try
-    {
-       const { data: user } = await octokit.users.getByUsername({
-            username: userHandle
-        })
-        return user;
-        // .then(({ user }) => {
-        //     return user
-        // });
-    }
-    catch(error)
-    {
-        var errorMessage = `Get user request: ${userHandle} - ${error} - ${error.status}`
-        res.render(path.join(__dirname + '/views/404.pug'), {error: errorMessage});
-        console.log(errorMessage)
-    }
+async function getUser (userHandle, res) {
+  try {
+    const { data: user } = await octokit.users.getByUsername({
+    username: userHandle
+    })
+    return user
+    // .then(({ user }) => {
+    //     return user
+    // });
+  } catch (error) {
+    const errorMessage = `Get user request: ${userHandle} - ${error} - ${error.status}`
+    res.render(path.join(__dirname + '/views/404.pug'), { error: errorMessage })
+    console.log(errorMessage)
+  }
 }
 
-function stop()
-{
-    app.port.clo
+function stop () {
+  app.port.clo
 }
 
 /**
  * Module Exports
  */
-module.exports = { getUser };
-
+module.exports = { getUser }
--- test/unit.test.js
+++ test/unit.test.js
@@ -1,21 +1,21 @@
-const { doesNotMatch } = require('assert');
-const assert = require('assert');
-const { exit } = require('process');
+const { doesNotMatch } = require('assert')
+const assert = require('assert')
+const { exit } = require('process')
 const Index = require('../index.js')
 
 describe('Validate Get User', () => {
- it('Should return a valid user - james-leha', async () => {
-       let userData = await Index.getUser('james-leha');
-       assert(userData);
-    });
- it('Should return a valid user - izzyco', async () => {
-        let userData = await Index.getUser('izzyco');
-        assert(userData);
-     });
-});
-
-after(async () => {  
-    console.log('Tests finished - exiting process...')
-    process.exit()
+  it('Should return a valid user - james-leha', async () => {
+    const userData = await Index.getUser('james-leha')
+    assert(userData)
   })
-//process.exit();
+  it('Should return a valid user - izzyco', async () => {
+    const userData = await Index.getUser('izzyco')
+    assert(userData)
+  })
+})
+
+after(async () => {
+  console.log('Tests finished - exiting process...')
+  process.exit()
+})
+// process.exit();
